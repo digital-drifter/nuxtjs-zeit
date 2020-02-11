c52dd40dacdbe496400788c5a13da9155ea1f788
Brian Lee <brianlee@friendemic.com>
2020-02-10 21:58:58 -0700

    firebase auth dropped in

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,6 +51,19 @@ export default {
     // Doc: https://github.com/nuxt-community/dotenv-module
     '@nuxtjs/dotenv'
   ],
+  /**
+   * Environment variables available to the client.
+   */
+  env: {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIRBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  },
   /*
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
